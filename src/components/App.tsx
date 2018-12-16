f2c907408d066954f4ac197fc11fa00022d09fbe
Eric Bach <bach.eric@gmail.com>
2018-12-16 10:57:39 -0700

    Updated

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,15 +10,39 @@ export class App extends React.Component<{}, IState> {
     };
   }
 
-  handleSubmit(e: any) {
+  public handleSubmit(e: React.FormEvent<HTMLFormElement>) : void {
     e.preventDefault();
     this.setState({
       currentTask: "",
-      tasks: [...this.state.tasks, this.state.currentTask]
+      // add this.state.currentTask to the array using the es6 spread operator
+      tasks: [...this.state.tasks, { id: this._timeInMillisconds(), value: this.state.currentTask, completed: false }]
     });
   }
 
-  render() {
+  public deleteTask(id: number) : void {
+    const tasks: Array<ITask> = this.state.tasks.filter((task: ITask) => task.id !== id);
+    this.setState( { tasks });
+  }
+
+  public toggleDone(index: number): void {
+    let task: ITask[] = this.state.tasks.splice(index, 1);
+    task[0].completed = !task[0].completed;
+    const currentTasks: ITask[] = [...this.state.tasks, ...task];
+    this.setState({tasks: currentTasks});
+  }
+
+  public renderTasks() : JSX.Element[] {
+    return this.state.tasks.map((task: ITask, index: number) => {
+      return ( 
+        <div key={task.id} className="tdl-task">
+          <span className={task.completed ? "is-completed" : ""}>{task.value}</span>
+          <button onClick={() => this.deleteTask(task.id)}>Delete</button>
+          <button onClick={() => this.toggleDone(index)}>{task.completed ? "Undo" : "Done"}</button>
+        </div> )
+    });
+  }
+
+  public render() : JSX.Element {
     console.log(this.state);
     return (
       <div>
@@ -27,17 +51,31 @@ export class App extends React.Component<{}, IState> {
         <form onSubmit={e => this.handleSubmit(e)}>
           <input
             type="text"
+            className="tdl-input"
             placeholder="Add a Task"
+            value={this.state.currentTask}
             onChange={e => this.setState({ currentTask: e.target.value })}
           />
           <button type="submit">Add Task</button>
         </form>
+        <section>{this.renderTasks()}</section>
       </div>
     );
+  }
+
+  private _timeInMillisconds(): number {
+    const date: Date = new Date();
+    return date.getTime();
   }
 }
 
 interface IState {
   currentTask: string;
-  tasks: Array<string>;
+  tasks: Array<ITask>;
+}
+
+interface ITask {
+  id: number;
+  value: string;
+  completed: boolean;
 }
